4f6a023af1fe271ec36ba93f100fa5322ba97bd5
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-08 15:20:54 +0530

    Extract markup into a functional component

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const page = (
-  <div>
-    <img src="/src/assets/react-logo.png" alt="" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>First released in 2013</li>
-      <li>Created by Jordan Walke</li>
-      <li>100K+ stars on Github</li>
-      <li>Maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps</li>
-    </ul>
-  </div>
-);
+
+function Page() {
+  const page = (
+    <div>
+      <img src="/src/assets/react-logo.png" alt="" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>First released in 2013</li>
+        <li>Created by Jordan Walke</li>
+        <li>100K+ stars on Github</li>
+        <li>Maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps</li>
+      </ul>
+    </div>
+  );
+
+  return page;
+}
 
 const rootEl = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(rootEl);
-root.render(<React.StrictMode>{page}</React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <Page />
+  </React.StrictMode>
+);
