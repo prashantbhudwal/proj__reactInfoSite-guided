c0050814fc125a5601859a54bd07da74acd2217c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-08 17:08:30 +0530

    Add Header markup and styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,13 @@
 import logo from "../assets/react-logo.png";
+import "./Header.css";
 
 function Header() {
   return (
     <header>
       <nav className="navBar">
         <img className="logo" src={logo} alt="" />
-        {/* <h2>ReactFacts</h2> */}
-        <ul className="menu">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
+        <h2 className="siteName">ReactFacts</h2>
+        <h3 className="siteDesc">My First React Project</h3>
       </nav>
     </header>
   );
