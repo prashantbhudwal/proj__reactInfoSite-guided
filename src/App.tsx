d37b5d9e545fc5dc0d3c9661d93f1e502a57be7a
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-08 17:08:13 +0530

    Add general styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "/src/index.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
+import "./App.css";
 
 function App() {
   return (
