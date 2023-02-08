e9b9165b83e274289946e21c77fdb922a58e8869
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-08 17:18:34 +0530

    Reorganize components into separate folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
 import "./App.css";
 
 function App() {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import logo from "../assets/react-logo.png";
+import logo from "/src/assets/react-logo.png";
 import "./Header.css";
 
 function Header() {
